ec48d0d26597a78634c2f33c36ebac62ab8dd66d
diego <diego@buildtruss.com>
2019-12-13 00:26:12 -0600

    Added logo svg and updated rule for image handling in webpack config.

--- webpack-common.js
+++ webpack-common.js
@@ -11,7 +11,7 @@ module.exports = {
         use: ["html-loader"]
       },
       {
-        test: /\.jpeg$/,
+        test: /\.(svg|jpg|png)$/,
         use: {
           loader: "file-loader",
           options: {
